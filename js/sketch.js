3448bbbc979b30a89e731db93df3a97eb665be6e
Marco Márquez <m.marquez.desarrollador@outlook.com>
2025-01-19 00:24:43 -0500

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,12 @@
-var video;
-var handPose;
-var hands = [];
+let video;
+let handPose;
+let hands = [];
+let painting;
+let px=0;
+let py=0;
 
 function preload() {
-    ml5.setBackend('webgl');
     handPose = ml5.handPose({ flipped: true });
-}
-
-function mousePressed(){
-    console.log(hands);
 }
 
 function gotHands(results) {
@@ -17,6 +15,8 @@ function gotHands(results) {
 
 function setup() {
     createCanvas(640, 480);
+    painting = createGraphics(640,480);
+    painting.clear()
     video = createCapture(VIDEO, { flipped: true });
     video.hide();
     handPose.detectStart(video, gotHands);
@@ -26,13 +26,19 @@ function draw() {
     image(video, 0, 0)
     if (hands.length > 0) {
         let hand = hands[0];
-        if (hand.confidence > 0.1) {
-            for (let i = 0; i < hand.keypoints.length; i++) {
-                let keypoint = hand.keypoints[i];
-                fill(255, 0, 255);
-                noStroke();
-                circle(keypoint.x, keypoint.y, 16);
-            }
+        let index = hand.index_finger_tip;
+        let thumb = hand.thumb_tip;
+        let d = dist(index.x,index.y,thumb.x,thumb.y);
+        let x = (index.x+thumb.x)*0.5;
+        let y = (index.y+thumb.y)*0.5;
+
+        if(d<25){
+            painting.stroke(0,0,0);
+            painting.strokeWeight(8);
+            painting.line(px,py,x,y);
         }
+        px=x;
+        py=y;
     }
+    image(painting,0,0)
 }
